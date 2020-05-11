7d4bbe317406bee228eb98bfd9397860b8e98bc2
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-05-10 22:43:21 -0700

    made some final changes to the application

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -10,18 +10,25 @@ class App extends React.Component {
     searchfield: "",
   };
 
+  componentDidMount = () => {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ robots: users }));
+  };
+
   onSearchChange = (e) => {
     this.setState({ searchfield: e.target.value });
   };
   render() {
-    const filterRobot = this.state.robots.filter((robot) => {
+    const { robots, searchfield } = this.state;
+    const filterRobot = robots.filter((robot) => {
       console.log(robot.name);
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    return (
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
       <div className="App">
         <h1>RoboFriends</h1>
         <Search search={this.onSearchChange} />
