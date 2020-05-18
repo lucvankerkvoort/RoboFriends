3f35ec0b5cc732e321303296e30845254131e626
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-05-18 16:08:37 -0700

    no changes made

--- robofriends/src/components/ErrorBoundry.js
+++ robofriends/src/components/ErrorBoundry.js
@@ -1,15 +1,16 @@
 import React from "react";
 
-class ErrorBoundry extends React.Component{
-    state={
-        hasError: false,
-    }
-    componentDidCatch=(error, info){
-        this.setState({hasError: true})
-    }
-    render(){
-        return(
-
-        )
-    }
+class ErrorBoundry extends React.Component {
+  state = {
+    hasError: false,
+  };
+  componentDidCatch = (error, info) => {
+    this.setState({ hasError: true });
+  };
+  render() {
+    const { hasError } = this.state;
+    return <p>Oops something went wrong</p>;
+  }
 }
+
+export default ErrorBoundry;
